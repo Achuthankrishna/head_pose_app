ca8f808138911904927ee3118494cc04b02fa77f
Achuthankrishna <achkrish07@gmail.com>
2024-02-27 16:31:04 -0500

    Record video to folder

--- utils.py
+++ utils.py
@@ -8,6 +8,16 @@ def record_response(question, response):
         file.write(f"Time: {timestamp}\n\n")
 
 def save_video(frames, filename):
+    if len(frames) == 0:
+        print("No frames to save.")
+        return
+    frame_h,frame_w,_=frames.shape[0]
+    output=cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'DIVX'), 30, (frame_w, frame_h))
+    for frame in frames:
+        output.write(frame)
+
+    output.release()
+    print(f"Video saved as {filename}")
 
 
 def record_video(cap, stframe, duration=3):
